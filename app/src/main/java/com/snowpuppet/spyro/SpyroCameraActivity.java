1ff2565c047a24c486f6ca6349c9ad0cf4912e69
Ashish Shekar <ckrackerx15@gmail.com>
2016-05-24 12:09:17 +0530

    Spyro: make full screen, minimum sdk 19

--- app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
+++ app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
@@ -1,7 +1,8 @@
 package com.snowpuppet.spyro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SpyroCameraActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class SpyroCameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spyro_camera);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        View windowDecor = getWindow().getDecorView();
+        if(hasFocus) {
+            windowDecor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 }
