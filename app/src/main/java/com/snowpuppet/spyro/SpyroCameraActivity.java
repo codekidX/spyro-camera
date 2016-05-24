7ca34fc74caf8da51a7e063479872d48b4fd2caf
Ashish Shekar <ckrackerx15@gmail.com>
2016-05-24 12:34:49 +0530

    Spyro-Base: cameraDevice intro

--- app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
+++ app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
@@ -1,15 +1,83 @@
 package com.snowpuppet.spyro;
 
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraDevice;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.TextureView;
 import android.view.View;
 
 public class SpyroCameraActivity extends AppCompatActivity {
+
+    TextureView spyroTextureView;
+
+    TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+    private CameraDevice mSpyroCamera;
+
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice cameraDevice) {
+            mSpyroCamera = cameraDevice;
+
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice cameraDevice) {
+            cameraDevice.close();
+            mSpyroCamera = null;
+
+        }
+
+        @Override
+        public void onError(CameraDevice cameraDevice, int i) {
+            cameraDevice.close();
+            mSpyroCamera = null;
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spyro_camera);
+
+        spyroTextureView = (TextureView) findViewById(R.id.spyro_texture_view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(spyroTextureView.isAvailable()) {
+        } else {
+            spyroTextureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeSpyroCamera();
     }
 
     @Override
@@ -25,5 +93,18 @@ public class SpyroCameraActivity extends AppCompatActivity {
             | View.SYSTEM_UI_FLAG_FULLSCREEN
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
+    }
+
+
+    /*
+     * App methods
+     */
+
+    private void closeSpyroCamera() {
+        if(mSpyroCamera !=null) {
+            mSpyroCamera.close();
+            mSpyroCamera = null;
+        }
+
     }
 }
