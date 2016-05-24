f713722caf8337ca333695d92fb5c263e07a44c8
Ashish Shekar <ckrackerx15@gmail.com>
2016-05-24 19:35:05 +0530

    Spyro-Base: configureTransform to transform textureView with rotation

--- app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
+++ app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
@@ -1,7 +1,10 @@
 package com.snowpuppet.spyro;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -9,7 +12,10 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,14 +23,20 @@ import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.snowpuppet.spyro.helpers.CompareSize;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -34,23 +46,35 @@ public class SpyroCameraActivity extends AppCompatActivity {
 
     private static final int REQUEST_CAMERA_PERMISSION = 1515;
     TextureView spyroTextureView;
+    ImageButton imageCaptureButton;
+
+    ImageReader mImageReader;
+
+    File imageFile;
+    File mSpyroFolder;
 
     private String mSpyroCameraId;
     private Size mPreviewSize;
 
     private CaptureRequest.Builder captureRequest;
 
+    private CameraCaptureSession mCameraCaptureSession;
+
+    private CameraCharacteristics mCameraCharacteristics;
+
+    private CameraManager mCamManager;
+
     TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
             spyroCameraSetup(i, i1);
-            connectSpyroCamera();
+            connectSpyroCamera(i,i1);
 
         }
 
         @Override
         public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
+            configureTransform(i,i1);
         }
 
         @Override
@@ -90,6 +114,13 @@ public class SpyroCameraActivity extends AppCompatActivity {
         }
     };
 
+    private ImageReader.OnImageAvailableListener imageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader imageReader) {
+            mSpyroHandler.post(new ImageSaver(imageReader.acquireNextImage(), imageFile));
+        }
+    };
+
     private HandlerThread mSpyroHandlerThread;
     private Handler mSpyroHandler;
 
@@ -108,6 +139,48 @@ public class SpyroCameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_spyro_camera);
 
         spyroTextureView = (TextureView) findViewById(R.id.spyro_texture_view);
+
+        imageCaptureButton = (ImageButton) findViewById(R.id.camera_shutter);
+
+        imageCaptureButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                try {
+                    final CaptureRequest.Builder captureRequest =
+                            mSpyroCamera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+
+                    captureRequest.addTarget(mImageReader.getSurface());
+
+                    // Use the same AE and AF modes as the preview.
+                    captureRequest.set(CaptureRequest.CONTROL_AF_MODE,
+                            CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+
+
+                    // Orientation
+                    int rotation = getWindowManager().getDefaultDisplay().getRotation();
+                    captureRequest.set(CaptureRequest.JPEG_ORIENTATION, spyroRotationSensor(mCameraCharacteristics,rotation));
+
+                    CameraCaptureSession.CaptureCallback CaptureCallback
+                            = new CameraCaptureSession.CaptureCallback() {
+
+                        @Override
+                        public void onCaptureCompleted(@NonNull CameraCaptureSession session,
+                                                       @NonNull CaptureRequest request,
+                                                       @NonNull TotalCaptureResult result) {
+                            File mFile = imageFile;
+                            Log.d("Spyro", mFile.toString());
+                        }
+                    };
+
+                    mCameraCaptureSession.stopRepeating();
+                    mCameraCaptureSession.capture(captureRequest.build(), CaptureCallback, null);
+
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -131,13 +204,6 @@ public class SpyroCameraActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    @Override
-    protected void onDestroy() {
-        closeSpyroCamera();
-        stopSpyroThread();
-        super.onDestroy();
-
-    }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -157,9 +223,16 @@ public class SpyroCameraActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_CAMERA_PERMISSION) {
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        mCamManager.openCamera(mSpyroCameraId, stateCallback, mSpyroHandler);
+                    }
 
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -171,12 +244,12 @@ public class SpyroCameraActivity extends AppCompatActivity {
 
 
     private void spyroCameraSetup(int width, int height) {
-        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+         mCamManager= (CameraManager) getSystemService(CAMERA_SERVICE);
 
         try {
             for (String camId :
-                    cameraManager.getCameraIdList()) {
-                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(camId);
+                    mCamManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics = mCamManager.getCameraCharacteristics(camId);
                 if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
                     break;
                 }
@@ -200,8 +273,10 @@ public class SpyroCameraActivity extends AppCompatActivity {
         }
     }
 
-    private void connectSpyroCamera() {
+    private void connectSpyroCamera(int width, int height) {
         CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        configureTransform(width,height);
 
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -222,7 +297,7 @@ public class SpyroCameraActivity extends AppCompatActivity {
 
     private void startSpyroPreview() {
         SurfaceTexture s = spyroTextureView.getSurfaceTexture();
-        s.setDefaultBufferSize(mPreviewSize.getWidth(),mPreviewSize.getHeight());
+        s.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
 
         Surface surface = new Surface(s);
 
@@ -234,6 +309,7 @@ public class SpyroCameraActivity extends AppCompatActivity {
             mSpyroCamera.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
                 @Override
                 public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                    mCameraCaptureSession = cameraCaptureSession;
                     try {
                         cameraCaptureSession.setRepeatingRequest(captureRequest.build(), null, mSpyroHandler);
                     } catch (CameraAccessException e) {
@@ -245,14 +321,14 @@ public class SpyroCameraActivity extends AppCompatActivity {
                 public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
 
                 }
-            },null);
+            }, null);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
     private void closeSpyroCamera() {
-        if(mSpyroCamera !=null) {
+        if (mSpyroCamera != null) {
             mSpyroCamera.close();
             mSpyroCamera = null;
         }
@@ -282,22 +358,91 @@ public class SpyroCameraActivity extends AppCompatActivity {
 
         deviceOrientation = orientationArray.get(deviceOrientation);
 
-        return (deviceOrientation + sensorOrientation + 360);
+        return (deviceOrientation + sensorOrientation + 270) % 360;
     }
 
     private static Size chooseOptimalPreviewSize(Size[] choices, int width, int height) {
         List<Size> bigEnough = new ArrayList<Size>();
 
-        for(Size options: choices) {
-            if(options.getHeight() == options.getWidth() *height/width &&
-                    options.getWidth() >= width && options.getHeight() >=height) {
+        for (Size options : choices) {
+            if (options.getHeight() == options.getWidth() * height / width &&
+                    options.getWidth() >= width && options.getHeight() >= height) {
                 bigEnough.add(options);
             }
         }
-        if(bigEnough.size() > 0) {
-            return Collections.min(bigEnough,new CompareSize());
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSize());
         } else {
             return choices[0];
+        }
+    }
+
+
+    /**
+     * Configures the necessary {@link android.graphics.Matrix} transformation to `mTextureView`.
+     * This method should be called after the camera preview size is determined in
+     * setUpCameraOutputs and also the size of `spyroTextureView` is fixed.
+     *
+     * @param viewWidth  The width of `spyroTextureView`
+     * @param viewHeight The height of `spyroTextureView`
+     */
+    private void configureTransform(int viewWidth, int viewHeight) {
+        Activity activity = SpyroCameraActivity.this;
+        if (null == spyroTextureView || null == mPreviewSize || null == activity) {
+            return;
+        }
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        Matrix matrix = new Matrix();
+        RectF viewRect = new RectF(0, 0, viewWidth, viewHeight);
+        RectF bufferRect = new RectF(0, 0, mPreviewSize.getHeight(), mPreviewSize.getWidth());
+        float centerX = viewRect.centerX();
+        float centerY = viewRect.centerY();
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
+            bufferRect.offset(centerX - bufferRect.centerX(), centerY - bufferRect.centerY());
+            matrix.setRectToRect(viewRect, bufferRect, Matrix.ScaleToFit.FILL);
+            float scale = Math.max(
+                    (float) viewHeight / mPreviewSize.getHeight(),
+                    (float) viewWidth / mPreviewSize.getWidth());
+            matrix.postScale(scale, scale, centerX, centerY);
+            matrix.postRotate(90 * (rotation - 2), centerX, centerY);
+        } else if (Surface.ROTATION_180 == rotation) {
+            matrix.postRotate(180, centerX, centerY);
+        }
+        spyroTextureView.setTransform(matrix);
+    }
+
+    private class ImageSaver implements Runnable {
+
+        Image mImage;
+        File mFile;
+
+        public ImageSaver(Image image, File file) {
+            this.mImage = image;
+            this.mFile = file;
+        }
+
+        @Override
+        public void run() {
+
+            ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            FileOutputStream output = null;
+            try {
+                output = new FileOutputStream(mFile);
+                output.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mImage.close();
+                if (null != output) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 }
