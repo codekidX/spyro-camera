6fc2ef866cdfd48e9e501eb3b1ef19da05f8b90b
Ashish Shekar <ckrackerx15@gmail.com>
2016-05-24 11:58:30 +0530

    Initial commit

---
+++ app/src/main/java/com/snowpuppet/spyro/SpyroCameraActivity.java
@@ -0,0 +1,13 @@
+package com.snowpuppet.spyro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpyroCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spyro_camera);
+    }
+}
